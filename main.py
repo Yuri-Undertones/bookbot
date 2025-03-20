ce418936008f3ff8d20b2b1934cb4f7692bca5a3
Bakyn <astrod@live.com>
2025-03-20 15:56:31 -0400

    created stats.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+from stats import count_words
+from stats import get_book_text
+
+def main(): 
+    filepath = "/home/andrue/workspace/github.com/Yuri-Undertones/bookbot/books/frankenstein.txt"
+    text = get_book_text(filepath)
+    word_count = count_words(text)
+    print(f"{word_count} words found in the document")
+
+main()
